b2308efcb9142b522344df2b1035522446bba21e
Evan Plaice <evanplaice@gmail.com>
2019-04-14 17:18:50 -0600

    Major refacor
    - 'title' attribute added
    - every attribute is observed and actively updated

--- src/wc-social.js
+++ src/wc-social.js
@@ -16,36 +16,41 @@ export class WCSocial extends HTMLElement {
     this.style.width = (this.style.width) ?  this.style.width : '32px';
     this.style.height = (this.style.height)? this.style.height : '32px';
   };
-  
-  async connectedCallback() {
-    this.network = (this.hasAttribute('network') ? this.getAttribute('network') : null);
-    if(this.network) {
-      this.setIcon();
-    } else {
-      throw Error(`WCSocial: 'network' attribue is require but missing`);
-    }
 
-    this.handle = (this.hasAttribute('handle') ? this.getAttribute('handle') : null);
-    this.href = (this.hasAttribute('href') ? this.getAttribute('href') : null);
-
-    if(this.handle || this.href) {
-      this.setLink();
-    }
+  static get observedAttributes() {
+    return ['network', 'href', 'handle', 'title'];
   }
 
-  setIcon() {
-    let href = '../assets/';
+  attributeChangedCallback(name, oldValue, newValue) {
+    this[name] = newValue;
+  }
+
+  async connectedCallback() {
+    this.network = this.hasAttribute('network') ? this.getAttribute('network') : null;
+    this.handle = (this.hasAttribute('handle') ? this.getAttribute('handle') : null);
+    this.href = this.hasAttribute('href') ? this.getAttribute('href') : null;
+    this.title = (this.hasAttribute('title') ? this.getAttribute('title') : null);
+  }
+
+  get network() { return this.getAttribute('network'); }
+  set network(value) {
+    // Edge: 'network' is required
+    if (!value) { throw Error(`WCSocial: 'network' attribue is require but missing`); }
+
+    // update attribute state
+    this.setAttribute('network', value);
 
     // lookup the svg url
-    switch (this.network) {
+    let href = '../assets/';
+    switch (value) {
       case 'github':
       case 'linkedin':
       case 'stackoverflow':
       case 'twitter':
-        href += `${this.network}.svg#${this.network}`;
+        href += `${value}.svg#${value}`;
         break;
       default:
-        throw Error(`WCSocial: network ${this.network} not supported`);
+        throw Error(`WCSocial: network ${value} not supported`);
     }
 
     // set the icon
@@ -53,41 +58,78 @@ export class WCSocial extends HTMLElement {
     use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', href);
   }
 
-  setLink() {
-    let href;
-    let title;
+  get handle() { return this.getAttribute('handle'); }
+  set handle(value) {
+    // Edge: 'handle' not set
+    if (value === null) { this.removeAttribute('handle'); return; }
 
-    // look up the network profile url
+    // update attribute state
+    this.setAttribute('handle', value);
+
+    let href;
     switch(this.network) {
       case 'github':
-        href = `https://github.com/${this.handle}`;
-        title = 'GitHub';
+        href = `https://github.com/${value}`;
         break;
       case 'linkedin':
-        href = `https://linkedin.com/in/${this.handle}`;
-        title = 'LinkedIn';
+        href = `https://linkedin.com/in/${value}`;
         break;
       case 'stackoverflow':
-        href = `https://stackoverflow.com/u/${this.handle}`;
-        title = 'StackOverflow'
+        href = `https://stackoverflow.com/u/${value}`;
         break;
       case 'twitter':
-        href = `https://twitter.com/${this.handle}`;
-        title = 'Twitter';
+        href = `https://twitter.com/${value}`;
         break;
-      default:
-        throw Error(`WCSocial: network ${this.network} not supported`);
     }
+    this.href = href;
+  }
 
-    // overrid the link if the 'href' attribute is set
-    if (this.href) {
-      href = this.href;
-    }
+  get href() {
+    return this.getAttribute('href');
+  }
+
+  set href(value) {
+    // Edge: 'href' not set
+    if (value === null) { this.removeAttribute('href'); return; }
+
+    // update attribute state
+    this.setAttribute('href', value);
 
     // set the link
     const a = this.shadowRoot.querySelector('a');
-    a.setAttribute('href', href);
-    a.setAttribute('title', `${title} link`);
+    a.setAttribute('href', value);
+  }
+
+  get title() {
+    return this.getAttribute('title');
+  }
+
+  set title(value) {
+    // Default: Set to '[network] link'
+    if (!value) {
+      switch(this.network) {
+        case 'github':
+          value = 'GitHub';
+          break;
+        case 'linkedin':
+          value = 'LinkedIn';
+          break;
+        case 'stackoverflow':
+          value = 'StackOverflow'
+          break;
+        case 'twitter':
+          value = 'Twitter';
+          break;
+      }
+      value += ' link';
+    }
+
+    // update attribute state
+    this.setAttribute('title', value);
+
+    // set the link
+    const a = this.shadowRoot.querySelector('a');
+    a.setAttribute('title', value);
   }
 
 }
